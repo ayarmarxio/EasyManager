b467f3522a876d2f1783d7869a140550b2bbecac
Mario Andres Ravina <mari81dh@edu.easj.dk>
2018-02-01 11:16:27 +0100

    PreMerging2

--- EasyManager/Model/EventCatalogSingleton.cs
+++ EasyManager/Model/EventCatalogSingleton.cs
@@ -9,22 +9,6 @@ namespace EasyManager.Model
 {
     class EventCatalogSingleton
     {
-        private static EventCatalogSingleton instance;
-
-        private EventCatalogSingleton() { }
-
-        public static EventCatalogSingleton Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new EventCatalogSingleton();
-                }
-                return instance;
-            }
-        }
-
-        //ObservableCollection<Event> EventCollection = new
+       
     }
 }
