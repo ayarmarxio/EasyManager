dc2f1426a32e7ab35196180f8b1543a70e6e049f
Mario Andres Ravina <mari81dh@edu.easj.dk>
2018-02-01 10:51:03 +0100

    EventClass1stDraft

--- EasyManager/Model/Event.cs
+++ EasyManager/Model/Event.cs
@@ -8,6 +8,26 @@ namespace EasyManager.Model
 {
     class Event
     {
+        private string _name;
+        private string _place;
+        private string _description;
+        private DateTime _time;
+        private DateTime _date;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Place { get => _place; set => _place = value; }
+        public string Description { get => _description; set => _description = value; }
+        public DateTime Time { get => _time; set => _time = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+
+        public Event ( string name, string place, string description, DateTime time, DateTime date)
+        {
+            name = _name;
+            place = _place;
+            description = _description;
+            time = _time;
+            date = _date;
+        }
 
     }
 }
