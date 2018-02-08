ed483ea3b255871854e38267c043d9fd32e1579f
Mario Andres Ravina <mari81dh@edu.easj.dk>
2018-02-08 18:12:49 +0100

    Avanzando

--- EasyManager/ViewModel/EventHandler.cs
+++ EasyManager/ViewModel/EventHandler.cs
@@ -1,44 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Email.DataProvider;
 using Windows.ApplicationModel.VoiceCommands;
+using Windows.Security.Cryptography.Core;
 using Windows.UI.Xaml;
 using EasyManager.Model;
+using Windows.UI.Popups;
 
 namespace EasyManager.ViewModel
 {
     class EventHandler
     {
         private EventViewModel _eventViewModel;
-        private EventCatalogSingleton _eventCatalog;
 
         public Event CreatedEvent { get; set; }
-        public EventCatalogSingleton EventCatalog { get; set; }
+   
         
-        public EventHandler( EventViewModel EventViewModel)
+        public EventHandler(EventViewModel EventViewModel)
         {
             _eventViewModel = EventViewModel;
         }
 
         public void CreateEvent()
         {
-             Event createdEvent = new Event (
-                _eventViewModel.Name, 
+            Event createdEvent = new Event(
+                _eventViewModel.Name,
                 _eventViewModel.Place,
-                _eventViewModel.Description, 
-                _eventViewModel.Time, 
-                _eventViewModel.Date
+                _eventViewModel.Description,
+                _eventViewModel.Date,
+                _eventViewModel.Time                 
                 );
 
+           _eventViewModel.EventCatalogSingleton.DoAddEvent(createdEvent);
 
-            // EventCatalog.DoAddEvent(createdEvent);
+        }
 
+        public async void DeleteEvent() 
+        {
+            if (_eventViewModel.SelectedEvent == null)
+            {
+                var messageDialog = new MessageDialog ("You need to select an Event in order to delete it");
+                await messageDialog.ShowAsync();
+            }
 
-            _eventViewModel.EventCatalogSingleton.DoAddEvent(CreatedEvent);
-
+            else
+            {
+                _eventViewModel.EventCatalogSingleton.DoDeleteEvent(_eventViewModel.SelectedEvent);
+            }
             
+           
         }
     }
 }
